d9fdd2cc6fd05007ddaf1cc152a1e1d5cdd1a637
Wolodymyr <wolodymyr.d.07@gmail.com>
2021-12-12 20:23:14 +0200

    Singleton

---
+++ Singleton/Singleton/DatabaseSample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class DatabaseSample
+    {
+        private static readonly Object _locker = new();
+
+        private static DatabaseSample Instance { get; set; }
+
+        private DatabaseSample()
+        {
+        }
+
+        public static DatabaseSample GetInstance()
+        {
+            if(Instance == null)
+            {
+                lock (_locker)
+                {
+                    // The second one, because first thread who had got there already created this instance, and the next one haven't to do that again
+                    if (Instance == null)
+                    {
+                        Instance = new DatabaseSample();
+                    }
+                }
+            }
+
+            return Instance;
+        }
+
+        // Not static because it should be accessible from a real object
+        public void Query(string query)
+        {
+            // Performs some complex logic
+            Console.WriteLine(query);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetHashCode()}";
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var database = DatabaseSample.GetInstance();
+            database.Query("SELECT * FROM Stock INNER JOIN Supplier ON SupplierId = Id");
+            var database2 = DatabaseSample.GetInstance();
+
+            Console.WriteLine($"Hash: {database} | {database2}");
+        }
+    }
+}
